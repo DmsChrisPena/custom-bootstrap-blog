5ebd4fa30684083c2113c4ffa598802c219f2ba8
Chris N Pena <Cpena1@Chriss-MacBook-Pro.local>
2015-06-18 12:02:34 -0500

    This blog is open source so have at it.

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// This is the jQuery to make the page scroll
+// All praise to jQuery!
+
+jQuery(document).ready(function($) { $("#scrollBlogpost").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#blogPost").offset().top
+	    }, 1200);
+	    return false;
+	});
+});
+
+jQuery(document).ready(function($) { $("#scrollBlogpost1").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#blogPost").offset().top
+	    }, 1700);
+	    return false;
+	});
+});
+
+jQuery(document).ready(function($) { $("#scrollBlogpost2").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#blogPost").offset().top
+	    }, 1700);
+	    return false;
+	});
+});
+
+jQuery(document).ready(function($) { $("#scrollBlogpost3").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#blogPost").offset().top
+	    }, 1700);
+	    return false;
+	});
+});
+
+jQuery(document).ready(function($) { $("#scrollBlogpost4").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#blogPost").offset().top
+	    }, 1700);
+	    return false;
+	});
+});
+
+jQuery(document).ready(function($) { $("#scrollBlogpost5").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#blogPost").offset().top
+	    }, 1700);
+	    return false;
+	});
+});
